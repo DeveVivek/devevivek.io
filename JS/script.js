03e3641fa6ee2d58ba53cd5207bfecc4c9f4fbd6
Vivek Kumar <code.vivekkumar@gmail.com>
2022-05-29 23:53:37 +0530

    navbar added

--- JS/script.js
+++ JS/script.js
@@ -32,3 +32,17 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}

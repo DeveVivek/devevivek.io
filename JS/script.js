ecc5b6c8e8a44176657ccb3b33016593195276b7
Vivek Kumar <code.vivekkumar@gmail.com>
2022-06-25 22:16:20 +0530

    links added

--- JS/script.js
+++ JS/script.js
@@ -46,3 +46,17 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+

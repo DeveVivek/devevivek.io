7f17bc61cf7301ee7296ba5292bc392fa37fef1d
Vivek Kumar <code.vivekkumar@gmail.com>
2022-06-25 22:45:58 +0530

    footer changed

--- JS/script.js
+++ JS/script.js
@@ -47,16 +47,16 @@ if(close){
     })
 }
 
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
+// const header = document.querySelector("[data-header]");
+// const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// });
 

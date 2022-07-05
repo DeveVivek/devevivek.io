a269e1ca59104f96736325a6320b311f46cc577b
Vivek Kumar <code.vivekkumar@gmail.com>
2022-07-05 12:02:15 +0530

    final updation

--- JS/script.js
+++ JS/script.js
@@ -66,3 +66,10 @@ if(close){
 //   }
 // });
 
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+
+  window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
+
+});

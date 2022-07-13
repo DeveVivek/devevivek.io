7b2a0c2ac2d877cd31140d828ff459c331bf7f95
Vivek Kumar <code.vivekkumar@gmail.com>
2022-07-13 20:47:14 +0530

    modal added

--- JS/script.js
+++ JS/script.js
@@ -73,3 +73,15 @@ window.addEventListener("scroll", function () {
   window.scrollY >= 10 ? header.classList.add("active") : header.classList.remove("active");
 
 });
+
+
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
